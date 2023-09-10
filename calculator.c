71ab79cb6164c57e60ac6c7fca1b2a1f8db5e9ef
kawthar <kawtharikram2020@gmail.com>
2023-09-10 17:19:11 +0100

    hello

--- calculator.c
+++ calculator.c
@@ -4,15 +4,15 @@ int main() {
     char operator;
     double num1, num2, result;
 
-    // Input operator (+, -, *, /)
+    printf("Welcome to the Simple Calculator!\n");
     printf("Enter an operator (+, -, *, /): ");
     scanf(" %c", &operator);
 
-    // Input two numbers
+    
     printf("Enter two numbers: ");
     scanf("%lf %lf", &num1, &num2);
 
-    // Perform the calculation based on the operator
+    
     switch (operator) {
         case '+':
             result = num1 + num2;
@@ -23,18 +23,7 @@ int main() {
         case '*':
             result = num1 * num2;
             break;
-        case '/':
-            if (num2 != 0) {
-                result = num1 / num2;
-            } else {
-                printf("Error: Division by zero!\n");
-                return 1; // Exit with an error code
-            }
-            break;
-        default:
-            printf("Error: Invalid operator!\n");
-            return 1; // Exit with an error code
-    }
+         }
 
     printf("Result: %.2lf\n", result);
 
